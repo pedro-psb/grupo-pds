746b23ad8ecae0dc41d49a07fbbe6223cfe17b00
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-04-14 15:15:46 -0300

    arvoreImprimir

--- atividades/tad-arvore/arvore.c
+++ atividades/tad-arvore/arvore.c
@@ -2,71 +2,96 @@
 #include <stdlib.h>
 #include "arvore.h"
 
-typedef struct No {
+typedef struct No
+{
   int valor;
   No *esq;
   No *dir;
 } No;
 
-typedef struct Arvore {
+typedef struct Arvore
+{
   No *raiz;
 } Arvore;
 
 /* Insere no recursivamente. */
 void _insereNo(No *novo, No *existente);
 
-Arvore *arvoreCria() {
+Arvore *arvoreCria()
+{
   Arvore *nova_arvore = malloc(sizeof(Arvore));
   // Checa alocação de memoria
-  if (nova_arvore == NULL){
+  if (nova_arvore == NULL)
+  {
     printf("Houve um erro ao alocar memoria para a arvore.\n");
     exit(1);
   }
-  nova_arvore -> raiz = NULL;
+  nova_arvore->raiz = NULL;
   return nova_arvore;
 }
 
-void arvoreInsere(Arvore *arvore, int valor) {
+void arvoreInsere(Arvore *arvore, int valor)
+{
   // Inicializa
-  No * novo_no = malloc(sizeof(No));
-  novo_no -> valor = valor;
-  novo_no -> esq = NULL;
-  novo_no -> dir = NULL;
+  No *novo_no = malloc(sizeof(No));
+  novo_no->valor = valor;
+  novo_no->esq = NULL;
+  novo_no->dir = NULL;
 
-  if(arvore->raiz == NULL){
+  if (arvore->raiz == NULL)
+  {
     arvore->raiz = novo_no;
-  } else {
+  }
+  else
+  {
     _insereNo(novo_no, arvore->raiz);
   }
 }
 
 /* Função auxiliar para inserir no recursivamente (arvore binaria) */
-void _insereNo(No *novo, No *existente) {
-  if(novo-> valor < existente -> valor) {
-    if (existente -> esq == NULL){
-      existente -> esq = novo;
-    } else {
-      _insereNo(novo, existente -> esq);
+void _insereNo(No *novo, No *existente)
+{
+  if (novo->valor < existente->valor)
+  {
+    if (existente->esq == NULL)
+    {
+      existente->esq = novo;
     }
-  } else if(novo-> valor > existente -> valor) {
-    if (existente -> dir == NULL){
-      existente -> dir = novo;
-    } else {
-      _insereNo(novo, existente -> dir);
+    else
+    {
+      _insereNo(novo, existente->esq);
+    }
+  }
+  else if (novo->valor > existente->valor)
+  {
+    if (existente->dir == NULL)
+    {
+      existente->dir = novo;
+    }
+    else
+    {
+      _insereNo(novo, existente->dir);
     }
   }
 }
 
 /* Checa se arvore binaria contem valor. 0=nao-contem, 1=contem */
-int arvoreBuscar(Arvore *arvore, int valor) {
+int arvoreBuscar(Arvore *arvore, int valor)
+{
   //
   No *aux = arvore->raiz;
-  while (aux != NULL){
-    if (aux->valor == valor) {
+  while (aux != NULL)
+  {
+    if (aux->valor == valor)
+    {
       return 1;
-    } else if (valor < aux->valor){
+    }
+    else if (valor < aux->valor)
+    {
       aux = aux->esq;
-    } else {
+    }
+    else
+    {
       aux = aux->dir;
     }
   }
@@ -74,17 +99,22 @@ int arvoreBuscar(Arvore *arvore, int valor) {
 }
 
 /* Apagar árvore */
-void arvoreApagar(Arvore* arvore, int x){
-  
+void arvoreApagar(Arvore *arvore, int x)
+{
+}
 
+void arvore_imprimir_no(No *no)
+{
+  if (no != NULL)
+  {
+    printf("%i ", no->valor);
+    arvore_imprimir_no(no->esq);
+    arvore_imprimir_no(no->dir);
+  }
 }
 
 /* Imprimir árvore */
-void arvoreImprimir(Arvore* arvore){
-  if(arvore->raiz){
-
-      arvore->raiz->esq;
-      printf("%d", arvore->raiz->valor);
-      arvore->raiz->dir;
-
-  }}
+void arvoreImprimir(Arvore *arvore)
+{
+  arvore_imprimir_no(arvore->raiz);
+}
--- atividades/tad-arvore/teste.c
+++ atividades/tad-arvore/teste.c
@@ -3,29 +3,29 @@
 
 int main()
 {
-    int num,contem=0;
-    Arvore *arvr= arvoreCria();
+    int num, contem = 0;
+    Arvore *arvr = arvoreCria();
     arvoreInsere(arvr, 1);
     arvoreInsere(arvr, 5);
     arvoreInsere(arvr, 7);
     arvoreInsere(arvr, -3);
     arvoreInsere(arvr, 12);
+    arvoreInsere(arvr, 15);
+    arvoreInsere(arvr, 2);
 
     printf("0=Falso, 1=Verdadeiro\n");
     printf("Arvore contem 7? %d\n", arvoreBuscar(arvr, 7));
     printf("Arvore contem 2? %d\n", arvoreBuscar(arvr, 2));
     printf("Arvore contem 5? %d\n", arvoreBuscar(arvr, 5));
 
-    
-    if(arvoreBuscar(arvr,num))
+    if (arvoreBuscar(arvr, num))
     {
-        //arvore_apagar(arvr,num);
+        // arvore_apagar(arvr,num);
     }
     else
     {
         printf("O valor não se encontra na arvore\n");
     }
-    
-    arvoreImprimir(arvr);
 
+    arvoreImprimir(arvr);
 }
